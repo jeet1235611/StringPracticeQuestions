b9b1937d2b3aa3dffdf7657ce3b1661834f80da6
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-21 01:55:01 +0530

    Create stringNullAndVoidConcept.cpp

---
+++ stringNullAndVoidConcept.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        const char* pointer = nullptr;
+        cout << "This is the value of pointer " << (void*)pointer << "." << endl;
+        return 0;
+}
